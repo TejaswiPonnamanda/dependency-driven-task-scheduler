4d25af587330cdf0592cc0d7a0a7c55e63002cf2
TejaswiPonnamanda <tejaswi555ponnamanda@gmail.com>
2025-04-13 18:03:59 +0530

    Added all the source code files

---
+++ src/main/java/com/signalx/task_scheduler/TaskSchedulerApplication.java
@@ -0,0 +1,14 @@
+package com.signalx.task_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Om srirama!!");
+		SpringApplication.run(TaskSchedulerApplication.class, args);
+	}
+
+}
